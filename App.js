45b42b7836f486a9177bb1394de75ec901b1cdd5
joshuamilton21191 <142074883+joshuamilton21191@users.noreply.github.com>
2023-09-23 10:35:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Expt6 from './Expt6';
+import Expt7 from './Expt7';
+import Expt8 from './Expt8';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Expt8/>
+    </div>
+  );
+}
+
+export default App;
